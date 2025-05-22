95bc59e517a96c0527ba0e37d88bd87c7c495bde
Rémi Bédard-Couture <remi.bc@outlook.com>
2025-05-22 11:00:27 -0400

    Fix path to collabora service

--- ct/collabora.sh
+++ ct/collabora.sh
@@ -28,7 +28,7 @@ function update_script() {
   header_info
   check_container_storage
   check_container_resources
-  if [[ ! -f /etc/systemd/system/coolwsd.service ]]; then
+  if [[ ! -f /usr/lib/systemd/system/coolwsd.service ]]; then
     msg_error "No ${APP} Installation Found!"
     exit
   fi
