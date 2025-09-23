25fddeb826407fc7d24a4edf8b05ae1846ccb418
Rémi Bédard-Couture <remi.bc@outlook.com>
2025-09-22 20:32:45 -0400

    Testing script for PR

---
+++ ct/frigate16.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/remz1337/ProxmoxVE/remz/misc/build.func)
+# Copyright (c) 2021-2025 tteck
+# Authors: tteck (tteckster) | Co-Author: remz1337
+# License: MIT | https://github.com/remz1337/ProxmoxVE/raw/remz/LICENSE
+# Source: https://frigate.video/
+
+APP="Frigate16"
+var_tags="${var_tags:-nvr}"
+var_cpu="${var_cpu:-4}"
+var_ram="${var_ram:-4096}"
+var_disk="${var_disk:-30}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-12}"
+var_unprivileged="${var_unprivileged:-0}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+    header_info
+    check_container_storage
+    check_container_resources
+    if [[ ! -f /etc/systemd/system/frigate.service ]]; then
+        msg_error "No ${APP} Installation Found!"
+        exit
+    fi
+    msg_error "To update Frigate, create a new container and transfer your configuration."
+    exit
+}
+
+start
+build_container
+description
+
+msg_info "Setting Container to Normal Resources"
+pct set $CTID -memory 2048
+pct set $CTID -cores 2
+msg_ok "Set Container to Normal Resources"
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:5000${CL}"
