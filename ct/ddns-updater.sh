f7af483a92b09a7bb06f564ffda0636d8dbf93f2
Rémi Bédard-Couture <remi.bc@outlook.com>
2026-06-24 10:56:36 -0400

    Add postfix satellite config to DDNS-Updater

--- ct/ddns-updater.sh
+++ ct/ddns-updater.sh
@@ -14,6 +14,7 @@ var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
 var_arm64="${var_arm64:-yes}"
 var_unprivileged="${var_unprivileged:-1}"
+var_postfix_sat="${var_postfix_sat:-yes}"
 
 header_info "$APP"
 variables
